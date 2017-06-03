5249bc6510cee7647aecf9155bb91d874199e755
Michael J. Simons <michael@simons.ac>
2017-06-03 11:59:21 +0200

    Remove all Spring Code from Domain code

--- payment_service/src/main/java/de/springbootbuch/messaging_jms/payment_service/Application.java
+++ payment_service/src/main/java/de/springbootbuch/messaging_jms/payment_service/Application.java
@@ -1,7 +1,18 @@
 package de.springbootbuch.messaging_jms.payment_service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jackson.JsonComponentModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.annotation.JmsListenerConfigurer;
+import org.springframework.jms.config.SimpleJmsListenerEndpoint;
+import org.springframework.jms.listener.adapter.MessageListenerAdapter;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
 
 /**
  * Part of springbootbuch.de.
@@ -14,5 +25,59 @@ public class Application {
 
 	public static void main(String[] args) throws InterruptedException {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	ObjectMapper objectMapper(final JsonComponentModule jsonComponentModule) {
+		final ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.registerModule(jsonComponentModule);
+		return objectMapper;
+	}
+
+	@Bean
+	FilmReturnedEventReceiver filmReturnedEventReceiver() {
+		return new FilmReturnedEventReceiver();
+	}
+
+	@Bean
+	MessageConverter filmReturnedEventConverter(final ObjectMapper jacksonObjectMapper) {
+		MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+		converter.setObjectMapper(jacksonObjectMapper);
+		converter.setTargetType(MessageType.TEXT);
+		converter.setTypeIdPropertyName("eventType");
+		final Map<String, Class<?>> f = new HashMap<>();
+		f.put("FilmReturnedEvent", FilmReturnedEvent.class);
+		converter.setTypeIdMappings(f);
+		return converter;
+	}
+
+	@Bean
+	MessageListenerAdapter filmReturnedEventListener(
+		final MessageConverter filmReturnedEventConverter,
+		final FilmReturnedEventReceiver filmReturnedEventReceiver
+	) {
+		final MessageListenerAdapter adapter
+			= new MessageListenerAdapter(
+				filmReturnedEventReceiver);
+		adapter
+			.setDefaultListenerMethod("filmReturned");
+		adapter
+			.setMessageConverter(filmReturnedEventConverter);
+		return adapter;
+	}
+
+	@Bean
+	JmsListenerConfigurer jmsListenerConfigurer(
+		MessageListenerAdapter filmReturnedEventListener
+	) {
+		return registrar -> {
+			final SimpleJmsListenerEndpoint rv
+				= new SimpleJmsListenerEndpoint();
+			rv.setId("returned-films-receiver");
+			rv.setMessageListener(filmReturnedEventListener);
+			rv.setDestination("returned-films-events");
+
+			registrar.registerEndpoint(rv);
+		};
 	}
 }
--- payment_service/src/main/java/de/springbootbuch/messaging_jms/payment_service/FilmReturnedEventReceiver.java
+++ payment_service/src/main/java/de/springbootbuch/messaging_jms/payment_service/FilmReturnedEventReceiver.java
@@ -1,12 +1,7 @@
 package de.springbootbuch.messaging_jms.payment_service;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import javax.jms.JMSException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.jms.annotation.JmsListener;
-import org.springframework.stereotype.Component;
 
 /**
  * Part of springbootbuch.de.
@@ -14,26 +9,12 @@ import org.springframework.stereotype.Component;
  * @author Michael J. Simons
  * @author @rotnroll666
  */
-@Component
 public class FilmReturnedEventReceiver {
 
 	private static final Logger LOG = LoggerFactory
-		.getLogger(Application.class);
+		.getLogger(FilmReturnedEventReceiver.class);
 
-	@JsonIgnoreProperties(ignoreUnknown = true)
-	public static class FilmReturnedEvent {
-
-		private final String title;
-
-		public FilmReturnedEvent(
-			@JsonProperty("title") String title
-		) {
-			this.title = title;
-		}
-	}
-
-	@JmsListener(destination = "returned-films-events")
-	public void filmReturned(FilmReturnedEvent event) throws JMSException {
+	public void filmReturned(FilmReturnedEvent event) {
 		LOG.info(
 			"Film '{}' returned, billing customer...", 
 			event.title
